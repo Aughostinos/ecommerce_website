55b135ff414ef253906be317c52cbb4cfc4737d4
Aughostinos <augustinos.nabil@gmail.com>
2024-10-29 03:37:56 +0300

    update user route to add: add/remove to/from wishList endpoints add/remove to/from Cart endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,4 +12,17 @@ userRouter.get('/profile', getProfile);
 // this should update the user's profile
 userRouter.put('/update-profile', updateProfile);
 
+// this should add a product to the user's wishlist
+userRouter.post('/add-to-wishlist', addToWishlist);
+
+// this should remove a product from the user's wishlist
+userRouter.delete('/remove-from-wishlist', removeFromWishlist);
+
+// this should add a product to the user's cart
+userRouter.post('/add-to-cart', addToCart);
+
+// this should remove a product from the user's cart
+userRouter.delete('/remove-from-cart', removeFromCart);
+
+
 export default userRouter;
