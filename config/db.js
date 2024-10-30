a69340ce766e9a65baeba6cf2bcf950b8d353c1b
Aughostinos <augustinos.nabil@gmail.com>
2024-10-30 04:57:08 +0300

    Remove Deprecated Options

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ mongoose.set('strictQuery', false);
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+    await mongoose.connect(process.env.MONGO_URI);
   console.log('Connected to MongoDB');
   } catch(err) {
     console.error('Could not connect to MongoDB', err);
