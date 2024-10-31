c42781edc0cf4120de0842ebfea4de5ff3de888e
Aughostinos <augustinos.nabil@gmail.com>
2024-10-31 03:09:50 +0300

    adding checkout route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,32 +1,36 @@
 import { Router } from "express";
 import { 
     getOrders,
-    addOrder,
-    updateOrder,
+    createOrder,
+    markOrderAsPaid,
     deleteOrder,
-    getOrder,
+    getOrderById,
     searchOrder
 } from '../controllers/orderController.js';
+import { authenticateUser, authorizeAdmin } from "../middleware/authMiddleware.js";
 
 const orderRouter = Router();
 
 //this should list all orders
-orderRouter.get('/get-orders', getOrders);
+orderRouter.get('/get-orders', authenticateUser, getOrders);
 
 //this should add an order
-orderRouter.post('/add-order', addOrder);
-
-//this should update an order
-orderRouter.put('/update-order/:id', updateOrder);
-
-//this should delete an order
-orderRouter.delete('/delete-order/:id', deleteOrder);
+orderRouter.post('/create-order', authenticateUser, createOrder);
 
 //this should get a single order
-orderRouter.get('/get-order/:id', getOrder);
+orderRouter.get('/:id', authenticateUser, getOrderById);
+
+//this should update an order
+orderRouter.put('/mark-as-paid/:id', authenticateUser ,markOrderAsPaid);
+
+//this should delete an order
+orderRouter.delete('/delete-order/:id', authenticateUser, deleteOrder);
+
+//this should get a single order
+orderRouter.get('/get-order/:id', authenticateUser, authorizeAdmin, getOrderById);
 
 //this should search for an order
-orderRouter.get('/search-order', searchOrder);
+orderRouter.get('/search-order', authenticateUser, searchOrder);
 
 export default orderRouter;
 
